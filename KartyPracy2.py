74084f09da515f0b469ef8fcd06d51a76a526772
1-b 33 Y. R <114579172+Roddeeeon212@users.noreply.github.com>
2022-10-05 11:19:48 +0000

    dodano 4 zadania

--- KartyPracy2.py
+++ KartyPracy2.py
@@ -1,2 +1,20 @@
 # zad 1
 
+# a=int(input("Podaj liczbe"))
+# if a %3 == 0:
+#    print("TAK")
+# else:
+#     print("Nie")
+
+
+# zad 2
+
+# a=int(input("Podaj liczbe: "))
+# if a>100 and a<1000 and a%17==0:
+#    print("TAK")
+# elze:
+#      print("Nie")
+
+# zad3
+
+# wiek+int(input("Ile masz lat"))
