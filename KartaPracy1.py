76bdee89a3225152d38b27e1c35d58309f984c38
1-b 33 Y. R <114579172+Roddeeeon212@users.noreply.github.com>
2022-10-05 10:57:33 +0000

    skonczono karty pracy 1

--- KartaPracy1.py
+++ KartaPracy1.py
@@ -1,3 +1,16 @@
+# zad 5 
+
+# a=int(input())
+# b=int(input())
+# print(2*(a+b)/5)
+
+# zadanie 6 
+
+# brutto+int(input())
+# print(brutto/1.23)
+
+
+# zad 4
 # a=int(input())
 # b=int(input())
 # print(a-b)
@@ -7,3 +20,7 @@
 # a,b,c=map(int,input().split())
 # print(a*b*c)
 
+# zad 
+a=int(input())
+b=int(input())
+print(a % b)
